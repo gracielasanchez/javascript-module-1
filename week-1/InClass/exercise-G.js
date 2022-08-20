c90becc0c845bf4de9701ac82297f156da5896ad
graciela sanchez <ruthgracisanchez@gmail.com>
2022-08-20 11:17:24 +0200

    ejercicio G

---
+++ week-1/InClass/exercise-G.js
@@ -0,0 +1,6 @@
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+const totalPeople = 23;
+
+console.log("Percentage students: " + Math.round((numberOfStudents * 100) / totalPeople) + "%");
+console.log("Percentage mentors: " + Math.round((numberOfMentors * 100) / totalPeople) + "%");
