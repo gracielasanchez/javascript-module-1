6d3ad7c1fe2fcc92d6852d907b63c0f26bfbd5eb
graciela sanchez <ruthgracisanchez@gmail.com>
2022-08-20 13:54:33 +0200

    ejercicios en clase

--- week-1/InClass/exercise-E.js
+++ week-1/InClass/exercise-E.js
@@ -1,4 +1,7 @@
 const greeting = "Hola"
 const name = "Graciela"
 const mensaje = `${greeting}, my name is ${name}`;
+const mensaje2 = greeting + ", my name is" + name
+
 console.log(mensaje);
+console.log(mensaje2)
--- week-1/InClass/exercise-I.js
+++ week-1/InClass/exercise-I.js
@@ -1,3 +1,6 @@
 function getAgeInDays(age) { return 2022 - age; }
-function createGreeting(name, age) { const ageInDays = getAgeInDays(age); const message = "Tu nombre es " + name + " y naciste en el año " + ageInDays + "."; return message; }
+function createGreeting(name, age) {
+    const ageInDays = getAgeInDays(age);
+    const message = "Tu nombre es " + name + " y naciste en el año " + ageInDays + "."; return message;
+}
 console.log(createGreeting("Graciela", 25));
