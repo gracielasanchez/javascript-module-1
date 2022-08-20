d2f5fe0052f0ad1aab8052e719d3d21dbe02d4b8
graciela sanchez <ruthgracisanchez@gmail.com>
2022-08-20 14:02:51 +0200

    ejercicio2 parameters

--- week-1/Homework/K-functions-parameters/exercise2.js
+++ week-1/Homework/K-functions-parameters/exercise2.js
@@ -1,5 +1,10 @@
 // Declare your function first
+function divide(number) {
+    // complete the function here
+    return number / 2;
+}
 
-var result = divide(3, 4);
+
+let result = divide(1.5);
 
 console.log(result);
