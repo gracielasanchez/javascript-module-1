1e0595ad2173c78f668d2a129ef2840f9df5313c
graciela sanchez <ruthgracisanchez@gmail.com>
2022-08-20 14:28:20 +0200

    ejercicio4 parametros

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,11 @@
 // Declare your function first
+function sume(num1, num2) {
+    return num1 + num2;
+
+}
 
 // Call the function and assign to a variable `sum`
+let resultadoSuma = sume(13, 124);
 
-console.log(sum);
+
+console.log(resultadoSuma);
