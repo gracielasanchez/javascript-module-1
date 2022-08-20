9f67186343e3b30df6c8bb08058a1f1f5139d734
graciela sanchez <ruthgracisanchez@gmail.com>
2022-08-20 10:57:54 +0200

    ejercicio f

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,7 @@
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+let total = numberOfStudents + numberOfMentors;
+
+console.log("Numero de estudiantes: " + numberOfStudents);
+console.log("Numero de Mentores: " + numberOfMentors);
+console.log("Numero toltal de estudiantes y mentores: " + total);
